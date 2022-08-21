dc5eb82a1a1f6d71304c479d2fc829a6af9c8aac
HakamShakir <hakamshakir2002@gmail.com>
2022-08-21 22:16:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let thanksArray = [];
+  let length = names.length;
+  for (let i = 0; i < names.length; i++) {
+    thanksArray[i] =
+      "Thank you, " + names[i] + ", for the wonderful " + event + " gift!";
+  }
+
+  return thanksArray;
+}
+
+function countDown(no) {
+  for (let i = no; i >= 0; i--) console.log(i);
+}
